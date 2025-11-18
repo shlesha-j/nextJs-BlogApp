910ffbb186ec96de2312aed97232f596ddd67e9d
Shlesha Joshi <shlesha_joshi@idealake.com>
2025-11-18 11:46:17 +0530

    initial commit

---
+++ app/BlogCreation/page.js
@@ -0,0 +1,62 @@
+"use client";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { addBlog } from "../utils/BlogApi";
+
+function BlogCreatePage() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm({ mode: "onChange" });
+
+  const onSubmit = async (data) => {
+    try {
+      await addBlog(data);
+      alert("Blog Added Successfully");
+      reset();
+    } catch (err) {
+      console.error("Error adding blog:", err);
+      alert("Something went wrong");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Blog Creation Page</h2>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-grp">
+          <label>Title: </label>
+          <input
+            {...register("title", {
+              required: "This field is required",
+              minLength: { value: 3, message: "Min 3 characters" },
+            })}
+          />
+          {errors.title && <p style={{ color: "red" }}>{errors.title.message}</p>}
+        </div>
+
+        <div className="form-grp">
+          <label>Description: </label>
+          <input
+            {...register("description", {
+              required: "This field is required",
+            })}
+          />
+          {errors.description && (
+            <p style={{ color: "red" }}>{errors.description.message}</p>
+          )}
+          
+        </div>
+
+        <button type="submit" disabled={isSubmitting}>
+          {isSubmitting ? "Submitting..." : "Add Blog"}
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default BlogCreatePage;
---
+++ app/utils/BlogApi.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:4000/blogs";
+
+
+export const fetchBlogs = async () => {
+  try {
+    const response = await axios.get(API_URL);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching blogs:", error);
+    throw error;
+  }
+};
+
+
+export const addBlog = async (data) => {
+  try {
+    const response = await axios.post(API_URL, data);
+    return response.data;
+  } catch (error) {
+    console.error("Error adding blog:", error);
+    throw error;
+  }
+};
